51d6a3ce0a0b0597a10694c5186dcb49dd28fca5
wilderjose <wilder.suarez@est.ulsa.edu.ni>
2026-07-11 14:32:49 -0600

    conductores

--- P_taxi/P_taxi/settings.py
+++ P_taxi/P_taxi/settings.py
@@ -364,12 +364,6 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ],
 
-    "DEFAULT_PAGINATION_CLASS": (
-        "rest_framework.pagination.PageNumberPagination"
-    ),
-
-    "PAGE_SIZE": 20,
-
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
